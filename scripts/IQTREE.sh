9c8f6834c8fc6372236e5fbfdfb023917e0ad9cd
aglucaci <alexander.lucaci@temple.edu>
2021-06-01 14:18:37 -0400

    hyphy updates

--- scripts/IQTREE.sh
+++ scripts/IQTREE.sh
@@ -4,12 +4,10 @@
 # FASTA variable is passed to the bash script via qsub -v
 IQTREE="/opt/iqtree/iqtree-1.6.6-Linux/bin/iqtree"
 NP=16
-
-#echo $IQTREE -v -s $FASTA -st CODON -nt AUTO -m GTR+I+G -nt $NP
-#$IQTREE -v -s $FASTA -st CODON -nt AUTO -m GTR+I+G -nt $NP
 OUTPUT_IQTREE="$FASTA".treefile
-echo "# Checking for $OUTPUT_IQTREE"
 
+
+echo "# Checking for $OUTPUT_IQTREE"
 if [ ! -s $OUTPUT_IQTREE ]; 
 then
    echo $IQTREE -s $FASTA -nt $NP -alrt 1000 -bb 1000
--- scripts/TN93.sh
+++ scripts/TN93.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N TN93
 
-FASTA=$1
+#FASTA=$1
 TN93="/home/aglucaci/tn93/tn93"
 $TN93 -t 1 -o $FASTA".dst" $FASTA
 exit 0
